25449f83af8c37d05653068c92e61472ef60d350
anarutesc <anarutesc@users.noreply.github.com>
2018-09-24 20:31:48 -0300

    Update CalcRPN.java

--- CalcRPN.java
+++ CalcRPN.java
@@ -3,29 +3,100 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projeto;
+package pratica4pilha;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
- * @author C6-PROF
+ * @author Ana Rute
  */
 public class CalcRPN {
+
     Pilha<Double> aPilha;
     Pilha<Operacao> hist;
-    
-    public CalcRPN(){
+
+    public CalcRPN() {
         aPilha = new Pilha<>();
         hist = new Pilha<>();
     }
-    
+
     public void mais() {
         Double a = aPilha.desempilha();
         Double b = aPilha.desempilha();
-        Double resultado = b+a;
+        Double resultado = b + a;
         aPilha.empilha(resultado);
     }
-    
-    public void cancela(){
-        
+
+    public void menos() {
+        Double a = aPilha.desempilha();
+        Double b = aPilha.desempilha();
+        Double resultado = b - a;
+        aPilha.empilha(resultado);
+    }
+
+    public void vezes() {
+        Double a = aPilha.desempilha();
+        Double b = aPilha.desempilha();
+        Double resultado = b * a;
+        aPilha.empilha(resultado);
+    }
+
+    public void dividido() {
+        Double a = aPilha.desempilha();
+        Double b = aPilha.desempilha();
+        Double resultado = b / a;
+        aPilha.empilha(resultado);
+    }
+
+    public Double resultado() {
+        return aPilha.topo();
+    }
+
+    void exec(String cmd) {
+        double x;
+        switch (cmd) {
+            case "+":
+                this.mais();
+                break;
+            case "-":
+                this.menos();
+                break;
+            case "*":
+                this.vezes();
+                break;
+            case "/":
+                this.dividido();
+                break;
+            case "clear":
+                aPilha.reinicialize();
+                break;
+            default:
+                x = Double.parseDouble(cmd);
+                aPilha.empilha(x);
+                break;
+        }
+    }
+
+    public static void interfaceUsuario() throws IOException {
+        CalcRPN calc = new CalcRPN();
+        String line;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) {
+                continue;
+            }
+            for (String s : line.split(" ")) {
+                calc.exec(s);
+            }
+            System.out.println("Pilha = " + calc.aPilha);
+        }
+        System.out.println("Ate logo");
+    }
+
+    public void cancela() {
+
     }
 }
